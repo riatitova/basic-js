a5a7eacb092d94c8ca0c2eb820622cb634737c75
Maria Titova <mary.titova.1995@gmail.com>
2020-10-27 21:57:51 +0300

    feat: solve vigenere

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -1,13 +1,105 @@
 //const CustomError = require("../extensions/custom-error");
+function isLetter(str) {
+  return str.length === 1 && str.match(/[a-z]/i);
+}
 
+function countNumber(num1, num2) {
+  let sum = num1 - 97 + (num2 - 97);
+  if (sum >= 26) {
+    sum = sum - 26;
+  }
+  return 97 + sum;
+}
+
+function countNumberDecrypt(num1, num2) {
+  let sum = num1 - 97;
+  let key = num2 - 97;
+  let res;
+  if (sum - key < 0) {
+    res = sum + 26 - key + 97;
+  } else {
+    res = sum - key + 97;
+  }
+  return res;
+}
 class VigenereCipheringMachine {
-  encrypt() {
+  constructor(bool) {
+    this.bool = bool;
+  }
+
+  encrypt(message, key) {
     //throw new CustomError('Not implemented');
     // remove line with error and write your code here
-  }    
-  decrypt() {
-   // throw new CustomError('Not implemented');
+    if (message === undefined || key === undefined) {
+      throw new Error("wrong value");
+    }
+    message = message.toLowerCase();
+    key = key.toLowerCase();
+    let newkey = [];
+    let answer = "";
+    let j = 0;
+    for (let i = 0; i < message.length; i++) {
+      if (j == key.length) {
+        j = 0;
+      }
+      if (isLetter(message.charAt(i))) {
+        newkey.push(key[j]);
+        j++;
+      }
+    }
+    newkey = newkey.join("");
+    j = 0;
+    for (let i = 0; i < message.length; i++) {
+      if (isLetter(message.charAt(i))) {
+        answer += String.fromCharCode(
+          countNumber(message.charCodeAt(i), newkey.charCodeAt(j))
+        );
+        j++;
+      } else {
+        answer += message.charAt(i);
+      }
+    }
+    if (this.bool == false) {
+      return answer.toUpperCase().split('').reverse().join('');
+    } else {
+      return answer.toUpperCase();
+    }
+  }
+
+  decrypt(message, key) {
+    // throw new CustomError('Not implemented');
     // remove line with error and write your code here
+    message = message.toLowerCase();
+    key = key.toLowerCase();
+    let newkey = [];
+    let answer = "";
+    let j = 0;
+    for (let i = 0; i < message.length; i++) {
+      if (j == key.length) {
+        j = 0;
+      }
+      if (isLetter(message.charAt(i))) {
+        newkey.push(key[j]);
+        j++;
+      }
+    }
+    newkey = newkey.join("");
+    j = 0;
+    for (let i = 0; i < message.length; i++) {
+      if (isLetter(message.charAt(i))) {
+        answer += String.fromCharCode(
+          countNumberDecrypt(message.charCodeAt(i), newkey.charCodeAt(j))
+        );
+        j++;
+      } else {
+        answer += message.charAt(i);
+      }
+    }
+    if (this.bool == false) {
+      return answer.toUpperCase().split('').reverse().join('');
+    } else {
+      return answer.toUpperCase();
+    }
   }
 }
 
